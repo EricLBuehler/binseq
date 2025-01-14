53f17a7e1ac5a42ece924437b9948d63de0445e6
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-01-14 11:43:25 -0800

    refactor: update all parallel impl to use tid

--- src/reader/single_mmap.rs
+++ src/reader/single_mmap.rs
@@ -185,9 +185,9 @@ impl MmapReader {
                         let record = record_set
                             .get_record(i)
                             .expect("Record should exist within range of set");
-                        processor.process_record(record)?;
+                        processor.process_record(record, thread_id)?;
                     }
-                    processor.on_batch_complete()?;
+                    processor.on_batch_complete(thread_id)?;
 
                     // Exit if we've processed our chunk
                     if finished && record_set.is_empty() {
